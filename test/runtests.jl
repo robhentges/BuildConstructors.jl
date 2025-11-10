c774453e97ac5d524285698373ed879767697479
Robert Hentges <robert.hentges@cern.ch>
2025-11-10 11:06:51 +0100

    Moved files from test to their appropriate places

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,9 +9,9 @@ cM_running_gw = ConstructorOfPRBModel(
     ConstructorOfFlatte(Fixed(-7.66), Running("g"), Fixed(1.88), (1.0, 2.6)),
     ConstructorOfCBpSECH(Fixed(0.002795), Fixed(2.48), Fixed(474), Fixed(8.1), Fixed(2.0), Fixed(1.3505), Fixed(0.5909), Running("w"), (-0.5, 0.5)),
     ConstructorOfPol1(Fixed(0.1), (1.0, 2.6)),
-    Fixed(0.5),
+    Running("fs"),
     (1.1, 2.5)
 )
 
-model = build_model(cM_running_gw, (g = 0.115, w = 0.5,))
+model = build_model(cM_running_gw, (g = 0.115, w = 0.5, fs = 0.5,))
 @test pdf(model, 1.1) ≈ 0.7084462317465321
